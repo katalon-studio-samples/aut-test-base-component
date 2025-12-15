1818ab1a0e89090ba18e27e25c894b89f3feb5d0
Huy Le <117883058+ktl-huyle@users.noreply.github.com>
2025-12-15 17:46:35 +0700

    Remove cssCodeSplit option from build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    cssCodeSplit: false,
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name].js',
